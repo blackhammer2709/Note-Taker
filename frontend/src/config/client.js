20583a7945fe0221d2f4e7c30815f494e0706a42
blackhammer2709 <rafaelalfonso67@gmail.com>
2024-02-10 21:48:14 -0400

    CORRIGIENDO CLIENTE AXIOS

--- frontend/src/config/client.js
+++ frontend/src/config/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //a req client to make http request more easily
 const AxiosCLient = axios.create({
-	baseURL: procces.env.API_URL || "https://notetakerapi-gxg0.onrender.com",
+	baseURL: "https://notetakerapi-gxg0.onrender.com",
 });
 
 export default AxiosCLient;
